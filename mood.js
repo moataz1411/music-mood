ec43edababde15dc327fdbde4071024bc9a86371
moataz1411 <mezoahmed1411@gmail.com>
2026-07-09 20:47:24 +0300

    add greeting message based on time of day and improve theme toggle button functionality

--- mood.js
+++ mood.js
@@ -77,3 +77,12 @@ function showMood(name){
         `;});
 document.getElementById("moodsong").innerHTML = html;
 }
+const btn=document.getElementById("themebtn");
+btn.onclick=()=>{
+    document.body.classList.toggle("dark");
+    if(document.body.classList.contains("dark")){
+        btn.innerHTML="☀️";
+    } else {
+        btn.innerHTML="🌙";
+    }
+}
--- script.js
+++ script.js
@@ -7,3 +7,15 @@ btn.onclick=()=>{
         btn.innerHTML="🌙";
     }
 }
+const hour =new Date().getHours();
+let greet="";
+if(hour<12){ 
+    greet="Good Morning";
+}
+else if (hour<18){
+    greet="Good Afternoon";
+}
+else{
+    greet="Good Evening";
+}
+document.getElementById("greeting").innerHTML=greet;
